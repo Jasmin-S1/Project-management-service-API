b3c10c2a582a5f22c19d9c17136cfac33f47ab19
Jasmin-S1 <101867431+Jasmin-S1@users.noreply.github.com>
2022-08-16 16:37:24 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    database_hostname: str      # praksa je da env. varijable budu velikim slovima ali ce pydantic automatski konvertovati u velika slova
+    database_hostname: str     
     database_port: str
     database_password: str  
     database_name: str 
